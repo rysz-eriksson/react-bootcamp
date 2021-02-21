d0cdc307348b82e2ad47fee7b677b9699c5f8542
ryszard wolosiuk <ryszard.wolosiuk@gmail.com>
2021-02-21 21:10:58 +0100

    basic webpack working setup

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <>
+            <p>
+                Let's test the setup using webpack and babel.
+            </p>
+            <p>
+                Testing hot reloading - looks like it works!
+            </p>
+        </>
+    )
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }, 
+  ]
+  },
+  //devtool: 'cheap-module-eval-source-map',
+  plugins: [new webpack.HotModuleReplacementPlugin()],
+  devServer: {
+    contentBase: path.resolve(__dirname, './public'),
+    hot: true,
+  }
+};
